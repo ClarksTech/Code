fb6290f2badc28ddc81ba0de89265b5904f80c36
ClarksTech <c.rutter50@gmail.com>
2021-11-19 12:44:51 +0000

    Optimised for single number - All sweeps possible

--- GA_Single_Number.py
+++ GA_Single_Number.py
@@ -5,7 +5,7 @@ from operator import add
 import matplotlib.pyplot as plt
 import functools
 import time
-
+import numpy as np
 
 #Create a member of the population
 def individual(length, min, max):
@@ -57,38 +57,7 @@ def evolve(pop, target, retain, random_select, mutate):
     parents.extend(children)
     return parents
 
-#default values
-target = 550
-#p_count = 100
-i_length = 6
-i_min = 0
-i_max = 100
-generations = 100
-retain=0.2
-random_select=0.05
-mutate=0.01
-
-
-runs_fitness_history = []
-iterations_needed_history = []
-p_count_history = []
-average_iteration_history = []
-runs_to_average = int(input("Enter number of runs to find average fitness from: "))
-show_generation_fitness_graph = input("Do you want Generational Fitness Graphs for every run? Y/N: ")
-show_average_fitness_variance_graph = input("Do you want to show fitness variance graphs? Y/N: ")
-show_average_iterations_needed_graph = input("Do you want to show iterations variance graphs? Y/N: ")
-
-#note start time of main program, so program run time can be know to find best trade off between time and performance
-start_time = time.time()
-
-for p_count in range (100, 1100, 100):
-    
-    sum_of_runs = 0
-    performance = 0
-    iterations_needed = 0
-    sum_of_iterations = 0
-    p_count_history.append(p_count)
-
+def sweep_parameter(p_count, retain, random_select, mutate, sum_of_iterations, iterations_needed, performance, sum_of_runs):
     for x in range(runs_to_average):
 
         #main usage
@@ -124,9 +93,6 @@ for p_count in range (100, 1100, 100):
             plt.plot(fitness_history)
             plt.show()
 
-    #diplay population size
-    print("Population size: ", p_count)
-
     #display average iterations needed to get solution
     average_iterations = sum_of_iterations/runs_to_average
     print("Average iterations for ", runs_to_average, " runs was: ", average_iterations)
@@ -143,6 +109,7 @@ for p_count in range (100, 1100, 100):
     #display run time of program
     print("Run time for ", runs_to_average, " runs was: ", (time.time()-start_time))
     print("Average run time: ", ((time.time()-start_time)/runs_to_average))
+    run_time_history.append((time.time()-start_time))
 
     if show_average_fitness_variance_graph == "Y":
         #plot graph of each runs final fitness to see variance
@@ -153,18 +120,146 @@ for p_count in range (100, 1100, 100):
         plt.show()
 
     if show_average_iterations_needed_graph == "Y":
-        #plot graph of iterations needed to see variance
+        #plot graph of iterations needed to see variance 
         plt.title("Final fitness of completed runs")
         plt.xlabel("Run")
         plt.ylabel('iterations needed')
         plt.plot(iterations_needed_history)
         plt.show()
+    return
 
-#plot graph of population sweep
-plt.title("Affect of population sweep on how quickly solution is found")
-plt.xlabel("population size")
-plt.ylabel('Average Iterations to find solution')
-plt.scatter(p_count_history,average_iteration_history)
-plt.show()
+def plot_swept_param(swept, swept_history):
+    #plot graph of population sweep
+    x = np.array(swept_history)
+    y = np.array(average_iteration_history)
+    theta = np.polyfit(x, y, 3)
+    print(f'The Parameters of the curve: {theta}')
+    y_line = theta[3] + theta[2] * pow(x, 1) + theta[1]  * pow(x, 2) +theta[0] * pow(x,3)
+
+    title = "Affect of " + swept + " on how quickly solution is found"
+    plt.title(title)
+    plt.xlabel(swept)
+    plt.ylabel('Average Iterations to find solution')
+    plt.scatter(x,y)
+    plt.plot(x, y_line, 'r')
+    #plt.scatter(x,run_time_history)
+    plt.show()
+    return
+
+#default values
+target = 550
+p_count = 100
+i_length = 6
+i_min = 0
+i_max = 100
+generations = 100
+retain=0.2
+random_select=0.05
+mutate=0.01
+
+run_time_history = []
+runs_fitness_history = []
+iterations_needed_history = []
+average_iteration_history = []
+runs_to_average = int(input("Enter number of runs to find average fitness from: "))
+show_generation_fitness_graph = input("Do you want Generational Fitness Graphs for every run? Y/N: ")
+show_average_fitness_variance_graph = input("Do you want to show fitness variance graphs? Y/N: ")
+show_average_iterations_needed_graph = input("Do you want to show iterations variance graphs? Y/N: ")
+
+#note start time of main program, so program run time can be know to find best trade off between time and performance
+start_time = time.time()
+
+if input("Least Iterations Solution? Y/N: ") == "Y":
+
+    target = 550
+    i_length = 6
+    i_min = 0
+    i_max = 100
+    generations = 100
+    retain=0.15
+    random_select=0.05
+    mutate=0.03
+    p_count = 600
+        
+    sum_of_runs = 0
+    performance = 0
+    iterations_needed = 0
+    sum_of_iterations = 0
+
+    sweep_parameter(p_count, retain, random_select, mutate, sum_of_iterations, iterations_needed, performance, sum_of_runs)
+
+
+if input("Sweep population? Y/N: ") == "Y":
+
+    p_count_history = []
+    target = 550
+    i_length = 6
+    i_min = 0
+    i_max = 100
+    generations = 100
+    retain=0.2
+    random_select=0.05
+    mutate=0.01
+    for p_count in range(100, 1100, 100):
+        
+        sum_of_runs = 0
+        performance = 0
+        iterations_needed = 0
+        sum_of_iterations = 0
+        p_count_history.append(p_count)
+
+        sweep_parameter(p_count, retain, random_select, mutate, sum_of_iterations, iterations_needed, performance, sum_of_runs)
+        
+    plot_swept_param("population", p_count_history)
+
+if input("Sweep Mutation probability? Y/N: ") == "Y":
+
+    mutate_history = []
+    target = 550
+    p_count = 600
+    i_length = 6
+    i_min = 0
+    i_max = 100
+    generations = 100
+    retain=0.2
+    random_select=0.05
+
+    for mutate in np.arange(0.01, 0.11, 0.01):
+        
+        sum_of_runs = 0
+        performance = 0
+        iterations_needed = 0
+        sum_of_iterations = 0
+        mutate_history.append(mutate)
+
+        sweep_parameter(p_count, retain, random_select, mutate, sum_of_iterations, iterations_needed, performance, sum_of_runs)
+        
+    plot_swept_param("mutation probability", mutate_history)
+
+if input("Sweep crossover probability? Y/N: ") == "Y":
+
+    crossover_history = []
+    target = 550
+    p_count = 600
+    i_length = 6
+    i_min = 0
+    i_max = 100
+    generations = 100
+    random_select= 0.05
+    mutate=0.03
+
+    for retain in np.arange(0.1, 0.4, 0.01):
+        
+        sum_of_runs = 0
+        performance = 0
+        iterations_needed = 0
+        sum_of_iterations = 0
+        crossover_history.append(retain)
+
+        sweep_parameter(p_count, retain, random_select, mutate, sum_of_iterations, iterations_needed, performance, sum_of_runs)
+        
+    plot_swept_param("crossover probability", crossover_history)
+
+
 
     
